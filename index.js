960fc45a0704e889c6b15add908476cd4a376b88
Megan <megan.nyakina@student.moringaschool.com>
2024-06-24 19:07:45 +0300

    adds arithmetic lab

--- index.js
+++ index.js
@@ -1,2 +1,30 @@
+//multiplication
 
-// Write your code here
+const num1 = 31;
+const num2 = 2;
+const multiply = num1 * num2 
+
+
+//random
+
+let random;
+function generateRandom(){
+    random=Math.floor(Math.random() * 100) + 1;
+}
+generateRandom();
+
+//mod 
+let num3 = 40;
+let num4 =6;
+let mod =(num3 % num4);
+
+//max
+let max;
+function generateMax(){
+    max=Math.max(1,2,3,4,20);
+}
+generateMax();
+
+
+
+
